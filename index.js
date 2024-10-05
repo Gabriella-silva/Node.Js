346ddd7be0b59c228c3bb53ed708c9461f06f23f
gabriella-silva <gabriellaferreira534@gmail.com>
2024-10-05 15:40:54 -0300

    iferjjf

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
+
+
+
+/*Importanto formulario de segurança par o uso de API */
 import http from 'http'
  const PORT= 3300;
-
+ /*Rotas locais para ver se o código funfa legal, "req": "res"*/
  const rotas ={
     '/':'Bateu legal',
     'gabbie':'Fiz de novo!'
  }
+
+ 
 const server = http.createServer((req,res)=>{
     res.writeHead(200, {'Content-Type' : 'text/plain'})
     res.end(rotas[req.url])
