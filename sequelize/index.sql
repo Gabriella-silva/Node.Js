bf3f8d66a841545a5ae15370abadbfb0ec84efa3
gabriella-silva <gabriellaferreira534@gmail.com>
2024-10-27 17:44:26 -0300

    .

---
+++ sequelize/index.sql
@@ -0,0 +1,31 @@
+/*APRENDI COMO MANIPULAR DADOS NO MYSQL APARTIR DO CMD*/
+
+
+CREATE TABLE users(
+    nome VARCHAR (32), 
+    email VARCHAR (100),
+    age INT
+);
+
+INSERT INTO users(nome,email,age) VALUES(
+    "gabbie",
+    "gabriellaferreira534@gmail.com",
+    18
+    
+);
+
+INSERT INTO users(nome,email,age) VALUES(
+    "emilly",
+    "teste@gmail",
+    13
+);
+
+INSERT INTO users(nome,email,age) VALUES(
+    "Salvuccio",
+    "fake@gmail",
+    21
+);
+DELETE FROM users WHERE nome = "gabbie";
+SELECT * FROM users Where age 21;
+
+UPDATE users SET nome = "bibi" WHERE nome = "gabbie" ;
---
+++ sequelize/teste.js
@@ -0,0 +1,3 @@
+const express = require("express");
+const rotas = express();
+const Sequelize = require("sequelize");
