90af86672255fa8a9f081c89b85ace77450fd610
gabriella-silva <gabriellaferreira534@gmail.com>
2024-10-27 14:09:10 -0300

    Usando nodemon e rotas

--- Rotas/main.js
+++ Rotas/main.js
@@ -4,6 +4,21 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Hello, World!"); // Envia uma resposta simples para a rota raiz.
 });
+ 
+
+app.get("/sobre", (req,res) => {
+
+  res.send("oiii, sou a bibi");
+});
+
+app.get("/seokjin", (req,res) => {
+
+  res.send("nice songgg")
+})
+
+app.get("/ola/:nome", (req,res) => {
+  res.send(req.params.nome+"<h1>Bem vindo</h1>")
+})
 
 app.listen(8081, () => {
   console.log("Servidor rodando na porta 8081");
